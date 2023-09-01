8bb108e9bb85ca2c3edc9b222039d5530baf65e2
guaitaxxb <beatrizguaita19@gmail.com>
2023-09-01 11:06:39 -0300

    Criada uma consulta que busca os números de usuarios por perfil

--- src/main/java/com/soulcode/goserviceapp/repository/ServicoRepository.java
+++ src/main/java/com/soulcode/goserviceapp/repository/ServicoRepository.java
@@ -1,6 +1,8 @@
 package com.soulcode.goserviceapp.repository;
 
 import com.soulcode.goserviceapp.domain.Servico;
+import com.soulcode.goserviceapp.domain.Usuario;
+import com.soulcode.goserviceapp.domain.enums.Perfil;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -12,9 +14,11 @@ public interface ServicoRepository extends JpaRepository<Servico, Long> {
 
     @Query(value =
             "SELECT s.*" +
-            " FROM servicos s" +
-            " JOIN prestadores_servicos ps ON s.id = ps.servico_id" +
-            " JOIN usuarios u ON u.id = ps.prestador_id" +
-            " WHERE u.email = ?", nativeQuery = true)
+                    " FROM servicos s" +
+                    " JOIN prestadores_servicos ps ON s.id = ps.servico_id" +
+                    " JOIN usuarios u ON u.id = ps.prestador_id" +
+                    " WHERE u.email = ?", nativeQuery = true)
     List<Servico> findByPrestadorEmail(String email);
 }
+
+
