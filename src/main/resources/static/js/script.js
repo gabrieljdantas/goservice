c5402d6f410faf7bb68aef819e6794e521464d8f
spilla-dev <137320211+spilla-dev@users.noreply.github.com>
2023-09-05 18:11:16 -0300

    Verificando data de Agendamento do serviço solicitado

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,20 @@
+var dataAtual = new Date().toISOString().split('T')[0];
+document.getElementById("data").min = dataAtual;
+
+
+function validarHorario() {
+ var dataNova = document.getElementById("data").value;
+ var horaSelecionada = document.getElementById("hora").value;
+ var horaAtual = new Date().toLocaleTimeString('en-US', {hour12: false});
+
+ if (horaSelecionada < horaAtual && dataNova <= dataAtual) {
+  alert("Por favor, selecione um horário válido.");
+  document.getElementById("hora").value = horaAtual;
+ }
+}
+
+document.getElementById("hora").addEventListener("change", validarHorario);
+
+var chat = document.querySelector('.chat');
+
+chat.scrollTop = chat.scrollHeight;
